85a039a4f3fadafa89bcb5393dbef94fb624da1a
Joemekka <mekka988@gmail.com>
2025-12-16 21:38:44 +0100

    ttt

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,6 +33,6 @@ CREATE TABLE Order_Details (
         order_id INT NOT NULL,
         book_id INT NOT NULL,
         quantity DOUBLE,
-        FOREIGN KEY order_id REFERENCES Orders(order_id) ON DELETE CASCADE ON UPDATE  CASCADE,
-        FOREIGN KEY book_id REFERENCES Books(book_id) ON DELETE CASCADE ON UPDATE CASCADE
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+        FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
